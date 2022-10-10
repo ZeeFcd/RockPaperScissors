8b2f81d80e7e94bcb025c31710382d2f2f4db799
ZeeFcd <ZeeFcd@protonmail.ch>
2022-10-10 18:55:13 +0200

    modified the image data provider in order to acces pictures easier

--- dataprovider.py
+++ dataprovider.py
@@ -6,27 +6,62 @@ class ImageTrainDataProvider:
 
     def __init__(self):
         # processed list contains the images as feature vectors
-        self.processed_paper_list = self.import_and_process('paper/*.png')
-        self.processed_rock_list = self.import_and_process('rock/*.png')
-        self.processed_scissors_list = self.import_and_process('scissors/*.png')
+        self.rock_list = None
+        self.paper_list = None
+        self.scissors_list = None
+        self.rock_feature_vectors = None
+        self.paper_feature_vectors = None
+        self.scissors_feature_vectors = None
+        self.processed_rock_list = None
+        self.processed_paper_list = None
+        self.processed_scissors_list = None
 
-    # gets and image and returns the processed image as a feature vector
+    def Read_All_Images(self):
+        self.rock_list = self.import_images('rock/*.png')
+        self.paper_list = self.import_images('paper/*.png')
+        self.scissors_list = self.import_images('scissors/*.png')
+
+    def Proccess_All_Images(self):
+        self.processed_rock_list = self.get_process_images(self.rock_list)
+        self.processed_paper_list = self.get_process_images(self.paper_list)
+        self.processed_scissors_list = self.get_process_images(self.scissors_list)
+
+    def Setup_Feature_Vectors(self):
+        self.rock_feature_vectors = []
+        self.paper_feature_vectors = []
+        self.scissors_feature_vectors = []
+        for rock in self.processed_rock_list:
+            self.rock_feature_vectors.append(np.array(rock).flatten())
+        for paper in self.processed_paper_list:
+            self.paper_feature_vectors.append(np.array(paper).flatten())
+        for scissors in self.processed_scissors_list:
+            self.scissors_feature_vectors.append(np.array(scissors).flatten())
+
+    # gets and image and returns the processed image
     def process_image(self, image):
 
-        Hue, Val, Sat = cv2.split(cv2.cvtColor(image, cv2.COLOR_BGR2HSV))
-        cut_out_by_Hue = cv2.threshold(Hue, 40, 255, cv2.THRESH_BINARY_INV)[1]
+        hsv_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+        hue = hsv_image[:, :, 0]
+        cut_out_by_hue = cv2.threshold(hue, 40, 255, cv2.THRESH_BINARY_INV)[1]
         kernel = np.ones((5, 5), np.uint8)
-        morph_opened_im = cv2.dilate(cv2.erode(cut_out_by_Hue, kernel, iterations=1), kernel, iterations=1)
+        morph_opened_im = cv2.dilate(cv2.erode(cut_out_by_hue, kernel, iterations=1), kernel, iterations=1)
         blurred = cv2.GaussianBlur(morph_opened_im, (21, 21), 0)
 
-        return np.array(cv2.threshold(blurred, 100, 255, cv2.THRESH_BINARY)[1]).flatten()
+        return cv2.threshold(blurred, 100, 255, cv2.THRESH_BINARY)[1]
 
-    # reads the images and processes them
-    def import_and_process(self, path):
+    # processes a list of images
+    def get_process_images(self, image_list):
+        proccessed=[]
+        for image in image_list:
+            proccessed.append(self.process_image(image))
+
+        return proccessed
+
+    def import_images(self, path):
 
         image_list = []
         for filename in glob.glob(path):
-            image_to_process = cv2.imread(filename)
-            image_list.append(self.process_image(image_to_process))
+            image = cv2.cvtColor(cv2.imread(filename), cv2.COLOR_BGR2RGB)
+            image_list.append(image)
 
         return image_list
--- main.py
+++ main.py
@@ -8,6 +8,14 @@ import dataprovider as proc
 if __name__ == "__main__":
 
     data_provider = proc.ImageTrainDataProvider()
-    print(data_provider.processed_paper_list[0])
-    print(data_provider.processed_rock_list[0])
-    print(data_provider.processed_scissors_list[0])
+    data_provider.Read_All_Images()
+    data_provider.Proccess_All_Images()
+    data_provider.Setup_Feature_Vectors()
+
+    plt.subplot(2, 3, 1), plt.imshow(data_provider.rock_list[0])
+    plt.subplot(2, 3, 2), plt.imshow(data_provider.paper_list[0])
+    plt.subplot(2, 3, 3), plt.imshow(data_provider.scissors_list[0])
+    plt.subplot(2, 3, 4), plt.imshow(data_provider.processed_rock_list[0], cmap='gray')
+    plt.subplot(2, 3, 5), plt.imshow(data_provider.processed_paper_list[0], cmap='gray')
+    plt.subplot(2, 3, 6), plt.imshow(data_provider.processed_scissors_list[0], cmap='gray')
+    plt.show()
