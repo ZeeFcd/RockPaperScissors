5608d965a07aecd5d7bca136f053764099b83fb1
ZeeFcd <ZeeFcd@protonmail.ch>
2022-10-17 22:34:19 +0200

    switched the way the program segments the hand, got rid of blur because of the resolution

--- test.py
+++ test.py
@@ -1,39 +1,38 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-import dataprovider as data
-from sklearn.svm import LinearSVC
-from joblib import dump, load
+
+from joblib import load
 
 kep = cv2.imread("testO.jpg")
 kep2 = cv2.cvtColor(kep, cv2.COLOR_BGR2RGB)
 
-hsv = cv2.cvtColor(kep, cv2.COLOR_BGR2HSV)
-Hue, Sat, Val = cv2.split(hsv)
-intensityValues, occurences=np.unique(Hue, return_counts=True)
-kezgray3 = cv2.threshold(Hue, 18, 255, cv2.THRESH_BINARY_INV)[1]
+ycrcb = cv2.cvtColor(kep, cv2.COLOR_BGR2YCR_CB)
+min_YCrCb = np.array([0, 133, 77], np.uint8)
+max_YCrCb = np.array([235, 173, 127], np.uint8)
+skinRegionYCrCb = cv2.inRange(ycrcb, min_YCrCb, max_YCrCb)
 
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(51,51))
+img_erosionk = cv2.erode(skinRegionYCrCb, kernel, iterations=3)
+img_dilationk = cv2.dilate(img_erosionk, kernel, iterations=3)
+szurt1 = cv2.GaussianBlur(img_dilationk, (21, 21), 0)
+atmeretez = cv2.resize(img_dilationk, (300, 200), interpolation=cv2.INTER_AREA)
+atmeretez[atmeretez > 0] = 255
 
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(21,21)) #np.ones((21, 21), np.uint8)
-img_erosionk = cv2.erode(kezgray3, kernel, iterations=7)
-img_dilationk = cv2.dilate(img_erosionk, kernel, iterations=7)
-szurt1 = cv2.GaussianBlur(img_dilationk,(21,21),0)
-atmeretez = cv2.resize(szurt1, (300, 200), interpolation=cv2.INTER_AREA)
+fvektor = np.array(atmeretez).flatten().reshape(1, -1)
 
-
-
-fvektor=np.array(atmeretez).flatten().reshape(1, -1)
-#fvektor=np.array(kezgray3).flatten().reshape(1, -1)
 clf = load('clfKNN.joblib')
+
 pred = clf.predict(fvektor)
 kopapirollo = {1: "Kő",  2: "Papír",  3:  "Olló"}
 
 plt.imshow(kep2)
 plt.show()
-plt.hist(intensityValues, intensityValues, weights=occurences)
+
+plt.imshow(skinRegionYCrCb, cmap='gray')
 plt.show()
-plt.imshow(kezgray3, cmap='gray')
+
+plt.imshow(atmeretez, cmap='gray'),  plt.title(kopapirollo[pred[0]])
 plt.show()
-plt.imshow(atmeretez, cmap='gray')
-plt.show()
-print(f"A képen látható: {kopapirollo[pred[0]]}")
+print(kopapirollo[pred[0]])
+
